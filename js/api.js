df791355958e1bc4a49912641037163d8bc93139
molokanova <v.molokanova@ktista.ru>
2022-07-29 10:54:09 +0300

    fix it

--- js/api.js
+++ js/api.js
@@ -1,12 +1,12 @@
 import { showMessage } from './util.js';
 
-const getDataError = 'Ошибка получения данных';
+const DATA_ERROR = 'Ошибка получения данных';
 
 export const getData = (onSuccess) => {
   fetch('https://26.javascript.pages.academy/kekstagram/data')
     .then((response) => response.json())
     .then((posts) => onSuccess(posts))
-    .catch(() => showMessage(getDataError));
+    .catch(() => showMessage(DATA_ERROR));
 };
 
 export const sendData = (onSuccess, onFail, body) => {
